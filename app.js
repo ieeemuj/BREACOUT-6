a747b84d987c39704cb0504075c5b071639d3725
AwesomeSam9523 <samakshgupta04@gmail.com>
2024-09-24 00:13:53 +0530

    test

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ app.use(express.json());
 
 app.use('/team', team);
 
+app.on('/', (req, res) => {
+    res.send('Hello World');
+});
+
 if (!process.env.VERCEL) {
   app.listen(process.env.PORT, () => {
       console.log(`Server is running on port ${process.env.PORT}`);

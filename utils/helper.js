16785c30ea0c704038602c4d5cc39daf644e52aa
AwesomeSam9523 <samakshgupta04@gmail.com>
2024-09-26 21:25:22 +0530

    test3

--- utils/helper.js
+++ utils/helper.js
@@ -1,16 +1,19 @@
 // Function to check if a point is within the geofence
 export function checkGeofence(geofence, point) {
   // const {lat, lan} = location;
-  const { coordinate1, coordinate2, coordinate3, coordinate4 } = geofence;
   // const minLat = Math.min(coordinate1.lat, coordinate2.lat, coordinate3.lat, coordinate4.lat);
   // const maxLat = Math.max(coordinate1.lat, coordinate2.lat, coordinate3.lat, coordinate4.lat);
   // const minlan = Math.min(coordinate1.lan, coordinate2.lan, coordinate3.lan, coordinate4.lan);
   // const maxlan = Math.max(coordinate1.lan, coordinate2.lan, coordinate3.lan, coordinate4.lan);
+  const { coordinate1, coordinate2, coordinate3, coordinate4 } = geofence;
   const polygon = [coordinate1, coordinate2, coordinate3, coordinate4];
   let odd = false;
   for (let i = 0, j = polygon.length - 1; i < polygon.length; i++) {
-    if (((polygon[i].lan > point.lan) !== (polygon[j].lan > point.lan))
-      && (point.lat < ((polygon[j].lat - polygon[i].lat) * (point.lan - polygon[i].lan) / (polygon[j].lan - polygon[i].lan) + polygon[i].lat))) {
+    if (
+      ((polygon[i][1] > point[1]) !== (polygon[j][1] > point[1]))
+      &&
+      (point[0] < ((polygon[j][0] - polygon[i][0]) * (point[1] - polygon[i][1]) / (polygon[j][1] - polygon[i][1]) + polygon[i][0]))
+    ) {
       odd = !odd;
     }
     j = i;

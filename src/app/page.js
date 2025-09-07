46a4ac502fa094a88f5b6885a7e65e719b02034f
AwesomeSam9523 <samakshgupta04@gmail.com>
2025-09-07 12:52:27 +0530

    url fix

--- src/app/page.js
+++ src/app/page.js
@@ -36,6 +36,8 @@ const Home = () => {
     }
 
     try {
+      alert('Breacout 6.0 has not started yet. Stay tuned for updates!');
+      return;
       setLoading(true);
       setErrorMessage(null);
 

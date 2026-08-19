c4e03e31b3ffa2f437ac500af5db82b2a86c0801
Mach808 <mananchoudhary1995@gmail.com>
2026-08-20 00:10:23 +0530

    Configure backend for local development

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {configDotenv} from "dotenv";
+import { configDotenv } from "dotenv";
 import cors from 'cors';
 import jwt from 'jsonwebtoken';
 
@@ -10,39 +10,57 @@ import admin from "./modules/admin.js";
 configDotenv();
 const app = express();
 app.use(cors({
-    origin: '*'
+  origin: '*'
 }));
+
 app.use(express.json());
+
+
 app.use((req, res, next) => {
   console.log(req.url);
-  if (req.url === '/team/login' || req.url.startsWith('/admin'))
-    return next();
 
-  const token = req.headers['authorization'];
-  if (!token) {
-    return res.status(403).json({success: false, message: "Token is required"});
+  if (req.url === '/team/login' || req.url.startsWith('/admin')) {
+    return next();
   }
 
-  jwt.verify(token.split(' ')[1], process.env.JWT_SECRET, (err, decoded) => {
-    if (err) {
-      return res.status(403).json({success: false, message: "Session expired! Please login again.", code: 1000});
+  const token = req.headers['authorization'];
+
+  if (!token) {
+    return res.status(403).json({
+      success: false,
+      message: "Token is required"
+    });
+  }
+
+  jwt.verify(
+    token.split(' ')[1],
+    process.env.JWT_SECRET,
+    (err, decoded) => {
+      if (err) {
+        return res.status(403).json({
+          success: false,
+          message: "Session expired! Please login again.",
+          code: 1000
+        });
+      }
+
+      req.team = decoded;
+      next();
     }
-    req.team = decoded;
-  });
-  next();
+  );
 });
 
 app.use('/team', team);
 app.use('/clue', clue);
 app.use('/admin', admin);
 
-app.on('/', (req, res) => {
-    res.send('Hello World');
+app.get('/', (req, res) => {
+  res.send('Hello World');
 });
 
 if (!process.env.VERCEL) {
-  app.listen(process.env.PORT, () => {
-      console.log(`Server is running on port ${process.env.PORT}`);
+  app.listen(process.env.BACKEND_PORT, () => {
+    console.log(`Server is running on port ${process.env.BACKEND_PORT}`);
   });
 }
 
